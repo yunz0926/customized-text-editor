e01ed7149bf726de804f0199faa154b89c42b348
yunz0926 <chldbswl9@gmail.com>
2022-10-13 17:03:20 +0900

    add editor wrapper

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,23 @@
 import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import styled from "styled-components";
 import TextEditor from "./components/TextEditor";
 
 function App() {
-  return <TextEditor />;
+  return (
+    <Wrapper>
+      <TextEditor />
+    </Wrapper>
+  );
 }
+
+const Wrapper = styled.div`
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  width: 700px;
+  height: 600px;
+  border: 1px solid;
+`;
 
 export default App;
